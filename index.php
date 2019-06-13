032655fdb0291c7b4f07f631a4bb6445a70190bf
Quoc Trung <trunghoang9599@gmail.com>
2019-06-13 15:17:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,16 @@
 
  		}
  		.picture {
- 			width: 50%;
+ 			width: 70%;
  			position: relative;
- 			height: 200px;
+ 			height: 400px;
+ 			float: right;
  		}
  		.menu {
+ 			width: 30%;
  			position: relative;
+ 			float: left;
+ 			border: 1px solid black;
  		}
  	</style>
  <?php echo '<p><strong>HELLO EVERYONE, THIS IS SECOND CLOUD APP</strong></p>'; 
@@ -24,17 +28,21 @@
  <h3>I'm Hoang Quoc Trung</h3>
  <h3>StudentID: GCD17073</h3>
  <h3>School: Greenwich Da Nang</h3>
- 	<img class="picture" src="https://img2.thuthuatphanmem.vn/uploads/2019/01/05/anh-doi-de-thuong-1_042809591.jpg">
- <section class="menu">
- <h2>Some functions with the database:</h2>
+ <div id="contnent">
+ 	<div class="picture">
+ 	<img src="https://img2.thuthuatphanmem.vn/uploads/2019/01/05/anh-doi-de-thuong-1_042809591.jpg">
+ 	</div>
+ 	<div class="menu">
+ 	<h2>Some functions with the database:</h2>
  	<br>
-     <strong>CONNECT DATABASE</strong><a href="ConnectToDB.php" target="_blank" >Click here</a>
+     <strong>CONNECT DATABASE</strong><a href="ConnectToDB.php" target="_blank" > Click here</a>
      <br/>
-     <strong>INSERT DATA TO THE DATABASE</strong><a href="InsertData.php" target="_blank" >Click here</a>
+     <strong>INSERT DATA TO THE DATABASE</strong><a href="InsertData.php" target="_blank" > Click here</a>
      <br/>
-     <strong>UPDATE DATA TO THE DATABASE</strong><a href="UpdateData.php" target="_blank" >Click here</a> 
+     <strong>UPDATE DATA TO THE DATABASE</strong><a href="UpdateData.php" target="_blank" > Click here</a> 
      <br/>
-     <strong>DELETE DATA TO THE DATABASE</strong><a href="DeleteData.php" target="_blank" >Click here</a> 
-    </section>
+     <strong>DELETE DATA TO THE DATABASE</strong><a href="DeleteData.php" target="_blank" > Click here</a> 
+     </div>
+ </div>
  </body>
 </html>
