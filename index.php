8057693ddf3aacf9144f4766d21a4a7fd6a231c1
Quoc Trung <trunghoang9599@gmail.com>
2019-06-25 22:13:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,6 @@
  			background: #ADD8E6;
 
  		}
- 		#infor {
- 			text-align: center;
- 		}
  		#content {
  			width: 100%;
  			margin: 0 auto;
@@ -27,24 +24,25 @@
  			bottom: 550px;
  		}
  	</style>
- <h1>HELLO EVERYONE, THIS IS ATNS CLOUD APP</h1>
- <br>
- <div id="infor">
+ <?php echo '<p><strong>HELLO EVERYONE, THIS IS ATNS CLOUD APP</strong></p>'; 
+ 
+ ?>
  <h2><strong>Dirrector:</strong> No name </h2>
  <h2><strong>Number ID:</strong> No name </h2>
  <h2><strong>Branch   :</strong> No name </h2>
- </div>
- <br>
  <div id="contnent">
+ 	<div class="picture">
+ 	<img src="https://sc01.alicdn.com/kf/HTB1u3cpsLuSBuNkHFqDq6xfhVXaG/Customized-vegetable-bean-plush-toy-cute-soft.jpg_350x350.jpg">
+ 	</div>
  	<div class="menu">
  	<h2>SOME FUNCTIONS DEMO: </h2>
  	<br>
      <strong>VIEW DATABASE</strong><a href="ConnectToDB.php" target="_blank" > Click here</a>
-     <br>
+     <br/>
      <strong>INSERT DATA TO THE DATABASE</strong><a href="InsertData.php" target="_blank" > Click here</a>
-     <br>
+     <br/>
      <strong>UPDATE DATA TO THE DATABASE</strong><a href="UpdateData.php" target="_blank" > Click here</a> 
-     <br>
+     <br/>
      <strong>DELETE DATA TO THE DATABASE</strong><a href="DeleteData.php" target="_blank" > Click here</a> 
      </div>
  </div>
