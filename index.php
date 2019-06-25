f3e1541f57ac4a00e5e04be807c1e407e2742eae
Quoc Trung <trunghoang9599@gmail.com>
2019-06-25 22:07:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,8 @@
  <body>
  	<style type="text/css">
  		body{
- 			background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzSApZc8cKHqmwIACBRkhFlD6YlPQYgB58Y8eGWaFK1VQk7Km3);
+ 			background: #ADD8E6;
 
- 		}
- 		#infor {
- 			text-align: center;
  		}
  		#content {
  			width: 100%;
@@ -27,7 +24,7 @@
  			bottom: 550px;
  		}
  	</style>
- <h1>HELLO EVERYONE, THIS IS ATNS CLOUD APP</h1>
+ <?php echo '<p><strong>HELLO EVERYONE, THIS IS ATNS CLOUD APP</strong></p>'; ?>
  <br>
  <h2><strong>Dirrector:</strong> No name </h2>
  <h2><strong>Number ID:</strong> No name </h2>
@@ -38,7 +35,7 @@
  	<img src="https://sc01.alicdn.com/kf/HTB1u3cpsLuSBuNkHFqDq6xfhVXaG/Customized-vegetable-bean-plush-toy-cute-soft.jpg_350x350.jpg">
  	</div>
  	<div class="menu">
- 	<h3>SOME FUNCTIONS DEMO: </h3>
+ 	<h2>SOME FUNCTIONS DEMO: </h2>
  	<br>
      <strong>VIEW DATABASE</strong><a href="ConnectToDB.php" target="_blank" > Click here</a>
      <br>
